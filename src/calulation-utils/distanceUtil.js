21baf173bee2c9caba4772bba3143b345cde4b1c
Yuval Shalgi <yuvals@perfectomobile.com>
2021-10-31 13:23:42 +0200

    Revert "temp changes"
    This reverts commit 90a86fd053a58b4716c780a4b3ffa0394f0683d4.

--- src/calulation-utils/distanceUtil.js
+++ src/calulation-utils/distanceUtil.js
@@ -1,5 +1,5 @@
 const random = require('canvas-sketch-util/random');
-import {AGENT_SPEED} from '../consts/drawConsts';
+import {SPEED} from '../consts/drawConsts';
 
 export const isOutOfRange = (num, limit) => {
     if (num >= limit -6 || num <= 0) return -1;
@@ -7,9 +7,5 @@ export const isOutOfRange = (num, limit) => {
 };
 
 export const agentSpeed = () => {
-    return random.range(-1*AGENT_SPEED,AGENT_SPEED)
+    return random.range(-1*SPEED,SPEED)
 };
-
-export const getDistance = (agentA, agentB) => (
-    Math.sqrt(agentA.pos.x* agentB.pos.x + agentA.pos.y* agentB.pos.y)
-)
