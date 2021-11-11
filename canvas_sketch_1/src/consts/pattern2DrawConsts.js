6b0ed6b9564b7b6a38e931564ef241208e39ff0e
Yuval Shalgi <yuvals@perfectomobile.com>
2021-11-11 19:43:51 +0200

    nicer pattern

--- canvas_sketch_1/src/consts/pattern2DrawConsts.js
+++ canvas_sketch_1/src/consts/pattern2DrawConsts.js
@@ -1,6 +1,6 @@
 export const LINE_WIDTH = 2;
-export const NUMBER_OF_AGENTS = 40;
+export const NUMBER_OF_AGENTS = 60;
 export const SPEED = 2;
 export const MIN_POINT_SIZE = 4;
 export const MAX_POINT_SIZE = 12;
-export const MAX_LINE_SIZE_PATTERN2 = 100;
+export const MAX_LINE_SIZE_PATTERN2 = 150;
