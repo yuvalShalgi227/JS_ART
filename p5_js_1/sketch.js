709b4c41451de816536787163d1f00bec73a7122
Yuval Shalgi <yuvals@perfectomobile.com>
2021-11-12 12:59:50 +0200

    Revert "outfile by MediaRecorder sucks"
    This reverts commit bd136629c64bc681529e394e815d562b977c9384.

--- p5_js_1/sketch.js
+++ p5_js_1/sketch.js
@@ -1,13 +1,35 @@
 const NUMBER_OF_AGENTS = 200;
 const BALL_SIZE = 48;
-let chunks = [];
-let frame =0;
+
 let balls= [];
-
-
 const randomRange = (min,max) => (
   Math.floor(Math.random() * (max - min + 1)) + min
 );
+
+
+//todo why is using a callback fucntion not working ?
+function setup() {
+  createCanvas(2080, 800);
+  for (let i =1; i <= NUMBER_OF_AGENTS; i++) {
+    balls.push(new Walker(randomRange(0,1080),randomRange(0,800)));
+  }
+}
+
+function draw() {
+  background(51);
+  let offset =1 ;
+  for (let i =1; i <= NUMBER_OF_AGENTS; i++) {
+    if (balls[i -1].w > 100) {
+      balls[i -1].setGrow(false);
+    }
+    if (balls[i -1].w < 10) {
+      balls[i -1].setGrow(true);
+    }
+    balls[i -1].walk(offset);
+    balls[i -1].display(offset);
+  }
+}
+
 class Walker {
   display = (offset) => {
     this.col[0] += this.grow && this.col[0] <255 ? offset: offset*-1;
@@ -27,18 +49,15 @@ class Walker {
     let mouse = createVector(mouseX, mouseY);
     this.acc = p5.Vector.sub(mouse, this.pos);
     this.acc.setMag(0.08);
-    
-   
+    //set limit on vel
+    let s = this.vel.mag;
+    this.vel.add(this.acc);
     if (this.vel.mag()>10) {
-      this.acc.rotate(-0.3);
       this.vel.setMag(1);
-    }else {
-      this.acc.rotate(0.3);
     }
     
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
 
+    this.pos.add(this.vel);
     if (!this.grow){
       offset = offset * -1;
     }
@@ -55,71 +74,7 @@ class Walker {
         this.h = randomRange(BALL_SIZE/10,BALL_SIZE);
         this.grow = true;
         this.col= [255, 204, 100]
+
     }
+
 }
-
-function exportVideo(e) {
-  var blob = new Blob(chunks);
-  var vid = document.createElement('video');
-  vid.id = 'recorded'
-  vid.controls = true;
-  vid.src = URL.createObjectURL(blob);
-  document.body.appendChild(vid);
-  vid.play();
-}
-
-class Recorder {
-  constructor() {
-    chunks.length = 0;
-    let stream = document.querySelector('canvas').captureStream(30),
-    recorder_temp = new MediaRecorder(stream);
-    this.recorder1 = recorder_temp;
-    this.recorder1.ondataavailable = e => {
-      if (e.data.size) {
-        chunks.push(e.data);
-      }
-    }
-  };
-  startRec = () => {
-    this.recorder1.start();
-  };
-  stopRec = () => {
-    this.recorder1.onstop = exportVideo;
-    this.recorder1.stop();
-  };
-};
-
-
-
-//todo why is using a callback fucntion not working ?
-function setup() {
-  createCanvas(2080, 800);
- 
-  for (let i =1; i <= NUMBER_OF_AGENTS; i++) {
-    balls.push(new Walker(randomRange(0,1080),randomRange(0,800)));
-  }
-}
-
-function draw() {
-  if (frame === 0){
-    let rec = new Recorder();
-    rec.startRec();
-  }
-  frame += 1;
-  if (frame>600) {
-    rec.stopRec();
-  }
-  background(51);
-  let offset =1 ;
-  for (let i =1; i <= NUMBER_OF_AGENTS; i++) {
-    if (balls[i -1].w > 100) {
-      balls[i -1].setGrow(false);
-    }
-    if (balls[i -1].w < 10) {
-      balls[i -1].setGrow(true);
-    }
-    balls[i -1].walk(offset);
-    balls[i -1].display(offset);
-  }
-}
-
