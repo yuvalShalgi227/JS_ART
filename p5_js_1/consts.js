1a5ceae499317c15e43d2b82bf5e1d6f4bb5fbdd
Yuval Shalgi <yuvals@perfectomobile.com>
2021-11-13 20:27:44 +0200

    project1 looks good

--- p5_js_1/consts.js
+++ p5_js_1/consts.js
@@ -1,4 +1,4 @@
-const NUMBER_OF_AGENTS = 200;
+const NUMBER_OF_AGENTS = 400;
 const BALL_SIZE = 48;
 let balls= [];
 let frame = 0 ;
--- p5_js_1/walker.js
+++ p5_js_1/walker.js
@@ -3,12 +3,11 @@ class Walker {
         this.pos = createVector(initx, inity)
         this.vel = createVector(0, 0);
         this.display = this.display.bind(this);
-        this.walk = this.walk.bind(this);
         this.w = random(BALL_SIZE/10,BALL_SIZE);
         this.h = random(BALL_SIZE/10,BALL_SIZE);
         this.grow = true;
         this.col= [255, 204, 100]
-    }
+    };
     display = (offset) => {
       this.col[0] += this.grow && this.col[0] <255 ? offset: offset*-1;
       this.col[1] += !this.grow && this.col[1] <150 ? offset: offset*-1;
@@ -25,28 +24,25 @@ class Walker {
     };
     updateVal(){
       this.vel.add(this.acc);
-      if (this.vel.mag()>10) {
-        this.vel.setMag(1);
-      }
-    }
+    };
     updateAcc() {
       let mouse = createVector(mouseX, mouseY);
       this.acc = p5.Vector.sub(mouse, this.pos);
       this.acc.setMag(0.08);
-    }
+    };
     normLimitsForces() {
       //rotate from size
-      if (this.vel.mag()>10) {
+      if (this.vel.mag()>100) {
         this.acc.rotate(-0.3);
-        this.vel.setMag(2);
+        this.vel.setMag(10);
       }else {
         this.acc.rotate(0.3);
       }
-    }
+    };
     setSize(offset) {
       this.w =  Math.abs(this.w + offset);
       this.h =  Math.abs(this.h + offset);
-    }
+    };
     walk = (offset) => {
       this.updateAcc();
       this.updateVal();
@@ -57,8 +53,18 @@ class Walker {
       if (!this.grow){
         offset = offset * -1;
       }
+      
+      this.edge();
       this.setSize(offset);
-  };
-
+    };
+    edge() {
+      if (this.pos.y < 0 || this.pos.y > height) {
+        this.vel.y *=-1;
+      }
+      if (this .pos.x < 0 || this.pos.x > width ) {
+        this.vel.x *=-1;
+      }
+      
+    }
   
   }
