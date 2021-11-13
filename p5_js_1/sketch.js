0e3e00bff0f4acabe7e9b9c04897f950bd850218
Yuval Shalgi <yuvals@perfectomobile.com>
2021-11-13 19:37:25 +0200

    i is 0

--- p5_js_1/sketch.js
+++ p5_js_1/sketch.js
@@ -1,7 +1,7 @@
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  for (let i =1; i <= NUMBER_OF_AGENTS; i++) {
+  for (let i =0; i <= NUMBER_OF_AGENTS; i++) {
     balls.push(new Walker(random(1080),random(800)));
   }
 }
@@ -10,14 +10,14 @@ function draw() {
   background(51);
   offX += 1;
   let offset =1 ;
-  for (let i =1; i <= NUMBER_OF_AGENTS; i++) {
-    if (balls[i -1].w > 100) {
-      balls[i -1].setGrow(false);
+  for (let i =0; i <= NUMBER_OF_AGENTS; i++) {
+    if (balls[i].w > 100) {
+      balls[i].setGrow(false);
     }
-    if (balls[i -1].w < 10) {
-      balls[i -1].setGrow(true);
+    if (balls[i].w < 10) {
+      balls[i].setGrow(true);
     }
-    balls[i -1].walk(offset);
-    balls[i -1].display(offset);
+    balls[i].walk(offset);
+    balls[i].display(offset);
   }
 }
